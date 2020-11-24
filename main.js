e148f6a59468b95309cf96324fe4d46807e74be0
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2020-11-24 16:23:41 +0100

    yo

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const product = 'Socks'
+
+
+const app = Vue.createApp({
+  data(){
+    return (
+      { product: 'Zocks',
+        description: 'Theze zockz are ze greatzes!'
+     });
+  }
+});
+
